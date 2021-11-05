1376545cf5204b6a0e823cab6798200dc9abcebc
yht0827 <yangheetae0827@naver.com>
2021-11-05 23:05:24 +0900

    - Boj 5397 [키로거] 스택, LinkedList 풀이

--- src/Boj/Boj5397.java
+++ src/Boj/Boj5397.java
@@ -9,13 +9,20 @@ public class Boj5397 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
     static int T;
 
     public static void main(String[] args) throws IOException {
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         T = Integer.parseInt(st.nextToken());
 
+        LinkedList();
+
+        // stack();
+    }
+
+    private static void LinkedList() throws IOException {
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
             List<Character> list = new LinkedList<>();
@@ -43,6 +50,41 @@ public class Boj5397 {
                 }
             }
             for (Character ch : list) sb.append(ch);
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void stack() throws IOException {
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            Stack<Character> left = new Stack<>();
+            Stack<Character> right = new Stack<>();
+
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                switch (ch) {
+                    case '<':
+                        if (!left.isEmpty()) right.push(left.pop());
+                        break;
+                    case '>':
+                        if (!right.isEmpty()) left.push(right.pop());
+                        break;
+                    case '-':
+                        if (!left.isEmpty()) left.pop();
+                        break;
+                    default:
+                        left.push(ch);
+                }
+            }
+            StringBuilder sb1 = new StringBuilder();
+            while (!left.isEmpty()) sb1.append(left.pop());
+            sb.append(sb1.reverse());
+            while (!right.isEmpty()) sb.append(right.pop());
 
             sb.append("\n");
         }
