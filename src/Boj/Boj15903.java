de6648555962f4b66cd3e0a426b9fd9161f7ad0e
yht0827 <yangheetae0827@naver.com>
2021-11-12 22:31:25 +0900

    - Boj 15903 [카드 합체 놀이]

--- src/Boj/Boj15903.java
+++ src/Boj/Boj15903.java
@@ -1,9 +1,37 @@
 package Boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Boj15903 {
 
-    public static void main(String[] args) {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, m;
+    static Queue<Long> pq = new PriorityQueue<>();
 
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) pq.offer(Long.parseLong(st.nextToken()));
+
+        while (m-- > 0) {
+            long sum = pq.poll() + pq.poll();
+            pq.offer(sum);
+            pq.offer(sum);
+        }
+
+        long result = 0L;
+        while (!pq.isEmpty()) result += pq.poll();
+
+        System.out.println(result);
     }
 
 }
