a2b8821d75a3822abcf5fb8173b394bca10a0ced
yht0827 <yangheetae0827@naver.com>
2021-11-13 18:29:27 +0900

    - Programmers 입실퇴실

--- src/Programmers/입실퇴실.java
+++ src/Programmers/입실퇴실.java
@@ -1,35 +1,33 @@
 package Programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class 입실퇴실 {
 
     public static void main(String[] args) {
 
-        int[] enter = {1, 3, 2};
-        int[] leave = {1, 2, 3};
+        int[][] enter = {{1, 3, 2}, {1, 4, 2, 3}, {3, 2, 1}, {3, 2, 1}, {1, 4, 2, 3}};
+        int[][] leave = {{1, 2, 3}, {2, 1, 3, 4}, {2, 1, 3}, {1, 3, 2}, {2, 1, 4, 3}};
 
-        System.out.println(Arrays.toString(solution(enter, leave)));
+        // [0,1,1], [2,2,1,3], [1,1,2], [2,2,2], [2,2,0,2]
+        for (int i = 0; i < enter.length; i++) System.out.println(Arrays.toString(solution(enter[i], leave[i])));
     }
 
     private static int[] solution(int[] enter, int[] leave) {
-        int[] answer = new int[enter.length];
-        List<Integer> list = new ArrayList<>();
-
+        int len = enter.length;
+        int[] answer = new int[len];
+        Map<Integer, Integer> map = new HashMap<>();
         int idx = 0;
-        for (int i = 0; i < answer.length; i++) {
-            list.add(enter[i]);
-            for (int j = 0; j < list.size(); j++) {
-                if (list.get(j) == enter[i]) {
-                    answer[list.get(j)-1] = list.size() - 1;
-                } else {
-                    answer[list.get(j)-1]++;
-                }
+
+        for (int j : enter) {
+            map.put(j, 0);
+            for (int key : map.keySet()) {
+                if (key == j) answer[key-1] = map.size()-1;
+                else answer[key-1]++;
             }
-            while (idx < answer.length && list.contains(leave[idx])) {
-                list.remove(Integer.valueOf(leave[idx++]));
+
+            while (idx < len && map.containsKey(leave[idx])) {
+                map.remove(leave[idx++]);
             }
         }
 
