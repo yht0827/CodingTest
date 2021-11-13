aa72505417b9decc4683d16d8ab20e6e5467c635
yht0827 <yangheetae0827@naver.com>
2021-11-14 03:36:54 +0900

    - Boj 10171 [고양이]

---
+++ src/Boj/Boj10171.java
@@ -0,0 +1,13 @@
+package Boj;
+
+public class Boj10171 {
+    public static void main(String[] args) {
+
+        String sb = "\\    /\\" + "\n" +
+                " )  ( ')" + "\n" +
+                "(  /  )" + "\n" +
+                " \\(__)|" + "\n";
+
+        System.out.println(sb);
+    }
+}
