9dd9700232a35fbd0c444136428931be8a32a63e
yht0827 <yangheetae0827@naver.com>
2021-11-26 22:40:55 +0900

    - Boj 2156 [포도주 시식]

--- src/Boj/Boj2156.java
+++ src/Boj/Boj2156.java
@@ -9,7 +9,7 @@ public class Boj2156 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N;
     static int[] dp = new int[10001];
-    static int[] arr = new int[10001];
+    static Integer[] arr = new Integer[10001];
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
@@ -18,8 +18,22 @@ public class Boj2156 {
 
         arr[0] = 0;
         arr[1] = dp[1];
-        arr[2] = Math.max(dp[0] + dp[1], dp[1] + dp[2]);
+        arr[2] = arr[1] + dp[2];
 
+        //System.out.println(topDown(N));
+        bottomUp();
+    }
+
+    private static int topDown(int n) {
+
+        if (arr[n] == null) {
+            arr[n] = Math.max(Math.max(topDown(n - 2), topDown(n - 3) + dp[n - 1]) + dp[n], topDown(n - 1));
+        }
+
+        return arr[n];
+    }
+
+    private static void bottomUp() {
         for (int i = 3; i <= N; i++)
             arr[i] = Math.max(arr[i - 1], Math.max(arr[i - 3] + dp[i - 1] + dp[i], arr[i - 2] + dp[i]));
 
