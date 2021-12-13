0960c9074de7a5477209dcc219ec603cd6f6d402
yht0827 <yangheetae0827@naver.com>
2021-12-13 18:30:03 +0900

    - Boj 11403 경로찾기

---
+++ src/Boj/Boj11403.java
@@ -0,0 +1,46 @@
+package Boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj11403 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int[][] map;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int j = 0; j < N; j++) {
+            for (int i = 0; i < N; i++) {
+                for (int k = 0; k < N; k++) {
+                    if (map[i][j] == 1 && map[j][k] == 1) {
+                        map[i][k] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
