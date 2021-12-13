0200501f2ddb99f9d1ee35b8968b150c9c164174
yht0827 <yangheetae0827@naver.com>
2021-12-13 20:19:37 +0900

    - Boj 2660 회장뽑기

---
+++ src/Boj/Boj2660.java
@@ -0,0 +1,23 @@
+package Boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2660 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N; // <=50
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1][N + 1];
+
+
+
+
+
+    }
+
+}
