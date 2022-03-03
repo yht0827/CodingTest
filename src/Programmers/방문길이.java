a899bef5a1767436dc4db49d71df0c8d12dddf92
yht0827 <yangheetae0827@naver.com>
2022-03-03 15:28:37 +0900

    Programmers 방문길이

---
+++ src/Programmers/방문길이.java
@@ -0,0 +1,58 @@
+package Programmers;
+
+public class 방문길이 {
+    public static void main(String[] args) {
+        String dirs = "ULURRDLLU";
+        //String dirs = "LULLLLLLU";
+        System.out.println(solution(dirs));
+    }
+
+    static int[] dir = {5, 5};
+    static boolean[][][][] map = new boolean[11][11][11][11];
+    static int answer = 0;
+
+    public static int solution(String dirs) {
+        for (String str : dirs.split(""))
+            move(str);
+        return answer;
+    }
+
+    public static void move(String str) {
+        int x = dir[0];
+        int y = dir[1];
+
+        switch (str) {
+            case "U":
+                if (++dir[1] > 10) {
+                    dir[1]--;
+                    return;
+                }
+                break;
+            case "L":
+                if (--dir[0] < 0) {
+                    dir[0]++;
+                    return;
+                }
+                break;
+            case "R":
+                if (++dir[0] > 10) {
+                    dir[0]--;
+                    return;
+                }
+                break;
+            default:
+                if (--dir[1] < 0) {
+                    dir[1]++;
+                    return;
+                }
+                break;
+        }
+
+        if (!map[x][y][dir[0]][dir[1]] && !map[dir[0]][dir[1]][x][y]) {
+            answer++;
+            map[x][y][dir[0]][dir[1]] = true;
+            map[dir[0]][dir[1]][x][y] = true;
+        }
+    }
+
+}
