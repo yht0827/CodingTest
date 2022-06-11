7ca3964de9bc9a255c8c9a8dad7fbfabe3831915
yht0827 <yangheetae0827@naver.com>
2022-06-11 17:12:20 +0900

    Programmers 전화번호 목록

---
+++ src/Programmers/전화번호_목록.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class 전화번호_목록 {
+
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+
+        System.out.println(solution(phone_book));
+    }
+
+    private static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
